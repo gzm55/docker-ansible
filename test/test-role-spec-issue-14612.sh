8f5a2a7b04eb773b869d2bc3dad45f45d9c6743b
James Z.M. Gao <gaozhiming@360.cn>
2017-04-02 21:41:41 +0800

    add test for issue 14612

---
+++ test/test-role-spec-issue-14612.sh
@@ -0,0 +1,6 @@
+echo
+echo "galaxy install should not always warn."
+
+ansible-galaxy -p roles install gzm55.require_disabe_become 2>&1 | tee test-role-spec-issue-14612.txt
+
+! grep '[DEPRECATION WARNING]: The comma separated role spec format' test-role-spec-issue-14612.txt
