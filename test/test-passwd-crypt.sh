a02a88ffb0e227bc8bcd712d09f457c351686604
James Z.M. Gao <gaozhiming@360.cn>
2017-10-27 08:01:51 +0800

    update test string for passwd-crypt

--- test/test-passwd-crypt.sh
+++ test/test-passwd-crypt.sh
@@ -3,5 +3,5 @@ echo "Check ability of crypting the password from prompt."
 
 echo "" \
 | ansible-playbook test-passwd-crypt.yml 2>&1 \
-| grep 'ERROR! Unexpected Exception: secret must be unicode or bytes, not None' \
+| grep 'ERROR! .*: secret must be unicode or bytes, not None' \
 && echo "See the expected exception."
